7a16a57e41aee7a7274edc8261fb261966e77ce9
vivekexceed-automation <63901762+vivekexceed@users.noreply.github.com>
2025-10-31 17:57:31 +0530

    Changes has been made as per Vivek review

--- src/main/java/com/qa/orangehrm/pages/ShoppingCartPg.java
+++ src/main/java/com/qa/orangehrm/pages/ShoppingCartPg.java
@@ -1,5 +1,8 @@
 package com.qa.orangehrm.pages;
 
+import org.openqa.selenium.By;
+
 public class ShoppingCartPg {
 
+	private final By dashboardPgHeader = By.cssSelector("h6.oxd-text");
 }
--- src/test/java/com/qa/orangehrm/test/ShoppingCartPgTest.java
+++ src/test/java/com/qa/orangehrm/test/ShoppingCartPgTest.java
@@ -1,5 +1,12 @@
 package com.qa.orangehrm.test;
 
+import org.testng.annotations.Test;
+
 public class ShoppingCartPgTest {
 
+	@Test
+	public void done() {
+		System.out.println("Done");
+	}
 }
+
