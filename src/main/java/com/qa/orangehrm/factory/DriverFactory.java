fd728c7556f2e7581114631d4bd0888cede0c347
vivekexceed-automation <63901762+vivekexceed@users.noreply.github.com>
2025-11-07 09:30:32 +0530

    added OR condition when env is null or qa

--- src/main/java/com/qa/orangehrm/factory/DriverFactory.java
+++ src/main/java/com/qa/orangehrm/factory/DriverFactory.java
@@ -72,7 +72,7 @@ public class DriverFactory {
 		log.info("env passed ========>"+ envName);
 
 		try {
-			if (envName == null) {
+			if (envName == null || envName.trim().toLowerCase() == "qa") {
 				log.warn("no env is passed, hence running test cases on QA env");
 				io = new FileInputStream("./src/test/resources/config/config.properties");
 			} else {
